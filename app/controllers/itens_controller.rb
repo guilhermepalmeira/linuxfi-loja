5032c72967c89ea328f903e184381562fa4bb5df
Guilherme Palmeira <guilhermepalmeira.java@gmail.com>
2010-07-14 22:03:35 -0300

    Aula 4 Rails

--- app/controllers/itens_controller.rb
+++ app/controllers/itens_controller.rb
@@ -8,12 +8,24 @@ class ItensController < ApplicationController
 
     session[:pedido_id] = pedido_atual.id#coloca o id do pedido na sessao
     
-    respond_to do |format|
+    ir_para_carrinho("Produto #{@produto.nome} adicionado com sucesso ao carrinho")
+    
+  end
+
+  def atualizar_pedido
+    pedido_atual.update_attributes(params[:pedido])
+    ir_para_carrinho("Carrinho atualizado com sucesso")
+  end
+
+  protected
+
+  def ir_para_carrinho(mensagem)
+        respond_to do |format|
       format.html do
-        flash[:aviso] = "Produto #{@produto.nome} adicionado com sucesso ao carrinho"
-        redirect_to produtos_path
+        flash[:aviso] = (mensagem)
+        redirect_to itens_path
       end
     end
   end
-
+ 
 end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,6 +1,10 @@
 class Pedido < ActiveRecord::Base
 
-  has_many :itens #um pedido tem vários itens
+  has_many :itens, :dependent => :destroy #um pedido tem vários itens, o :dependent, destroy o item se ele for zero
+  accepts_nested_attributes_for :itens #vai aceitar receber os atributos de forma aninhada
+  #vai atumaticamente criar no seu objeto um metodo "itens_attributes", criadno um array de hashs"
+  #def itens_attributes=(array)
+  after_save :remover_itens_zerados#gancho pra remover do carrinho qdo for zerado
 
   def adicionar_produto(produto, quantidade)
     if item = self.itens.detect { |i| i.produto == produto }
@@ -8,6 +12,28 @@ class Pedido < ActiveRecord::Base
     else
       self.itens.build( :produto_id => produto.id, :quantidade => quantidade )#se nao, cria um novo objeto do tipo item atravez do metodo build
     end
+  end
+
+  def preco_total
+    self.itens.to_a.sum( &:preco_total )#metodo to_a convete pra array
+    #self.itens.to_a.sum{ |item| item.preco_total }# a mesma coisa do codigo de cima, o & o substitui
+  end
+
+  def blank?#pergunta se o objeto ta vazio
+    self.itens.blank?
+  end
+
+  protected
+
+  def remover_itens_zerados
+    itens_a_remover = []
+    self.itens.each do |item|
+      if item.quantidade.blank? || item.quantidade < 1
+        itens_a_remover << item
+      end
+    end
+    self.itens.delete( *itens_a_remover )
+    true
   end
 
 end
