b4ece1806ec5a1f80c75ffd34c3cd04e8aee265c
Jodavid <jodavid.arts@gmail.com>
2010-07-26 21:25:41 -0300

    Commit no master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
+  #este é o master
+
   helper_method :pedido_atual#pra o metodo ficar disponivel pras views
 
   protected
