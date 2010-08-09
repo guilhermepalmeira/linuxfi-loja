d0c5d62932d84168b4fab110240b684803ffb403
Guilherme Palmeira <guilhermepalmeira.java@gmail.com>
2010-08-09 20:16:03 -0300

    Adicionando validação de numeros em quantidade de itens

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ActiveRecord::Base
   belongs_to :produto, :class_name => "Produto", :foreign_key => "produto_id" #um item pertence a um produto(nao eh necessario o que vem depois da virgula, pq produto eh mesmo nome de classe
   belongs_to :pedido #um item pertence a um pedido
 
+  validates_numericality_of :quantidade
+
   def preco_total
     self.quantidade * self.produto.preco
   end
