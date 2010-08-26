4946a530be08706fb79be557a531f7857965f504
Guilherme Palmeira <guilhermepalmeira.java@gmail.com>
2010-08-25 21:43:53 -0300

    Comentando solr

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,13 +2,13 @@
 class Produto < ActiveRecord::Base #o nome dessa classe no plural que vai ser a tabela do BD
 
   #set_table_name 'produto' # se quiser setar outro nome pra tabela do BD
-
+=begin
   searchable do
     text :nome, boost => 2.0
     text :descricao
     float :preco
   end
-
+=end
   validates_presence_of :nome, :preco
   validates_numericality_of :preco, :greater_than => 0, :allow_nil => true
 
